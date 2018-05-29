f50d95287b68e7ce6426957746614801685460c6
Fahad Hamid <fahad.hamid.gr@dartmouth.edu>
2018-05-29 04:05:36 -0400

    merged monitoring with peer:

--- peer/peer.h
+++ peer/peer.h
@@ -41,4 +41,14 @@ typedef struct segment_tracker{
 
 void peer_stop();
 
+#define MAX_LEN 1024 /*Path length for a directory*/
+#define MAX_EVENTS 1024 /*Max. number of events to process at one go*/
+#define LEN_NAME 16 /*Assuming that the length of the filename won't exceed 16 bytes*/
+#define EVENT_SIZE  ( sizeof (struct inotify_event) ) /*size of one event*/
+#define BUF_LEN     ( MAX_EVENTS * ( EVENT_SIZE + LEN_NAME )) /*buffer to store the data of events*/
+
+void *monitor(void *arg);
+void add_watches(int fd, char *root);
+
+
 #endif //PEER_H
